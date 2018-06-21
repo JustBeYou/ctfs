04b02c01665de73f28264de73d5d09d551259731
JustBeYou <mihailferaru2000@gmail.com>
2018-06-21 12:21:53 +0300

    Debug script for server

---
+++ HTB/pwn/oldbridge_server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template ./exe/oldbridge --host 88.198.233.174 --port 32922
+from pwn import *
+
+# Set up pwntools for the correct architecture
+exe = context.binary = ELF('./exe/oldbridge')
+
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
+# ./exploit.py GDB HOST=example.com PORT=4141
+host = args.HOST or '88.198.233.174'
+port = int(args.PORT or 32922)
+
+def local(argv=[], *a, **kw):
+    '''Execute the target binary locally'''
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+def start(argv=['1234'], *a, **kw):
+    '''Start the exploit against the target.'''
+    return local(argv, *a, **kw)
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+gdbscript = '''
+set follow-fork-mode child
+continue
+'''.format(**locals())
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+# Arch:     amd64-64-little
+# RELRO:    Partial RELRO
+# Stack:    Canary found
+# NX:       NX enabled
+# PIE:      PIE enabled
+
+context.terminal = ["xfce4-terminal", "-e"]
+io = start()
+io.interactive()
+
